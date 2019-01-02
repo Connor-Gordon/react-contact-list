478acebeafd01da3522dfe520b4d26561cfd1208
Connor Gordon <con.gordon@gmail.com>
2019-01-02 13:39:25 -0800

    updated to one page

--- src/App.js
+++ src/App.js
@@ -12,22 +12,9 @@ class App extends Component {
   render() {
     return (
       <Router>
-      <div id="container">
-          <div id="list">
-            <h3 id="h3">My Contacts</h3>
-
-            <ul>
-              {contacts.map(contact => (
-                <li className="li" key={'contact' + contact.id}>
-                  <Link to={"/Person/" + contact.id}><img id="thumbnail" src={contact.picture.thumbnail}/></Link>
-                  <span className="idk"><Link to={"/Person/" + contact.id}>{contact.name.first + " " + contact.name.last}</Link></span>
-                </li>
-                ))}
-            </ul>
-          </div>
-          <div>
+        <div id="container">
+            <Route exact path="/" component={Home} />
             <Route path="/Person/:id" component={Person} />
-          </div>
         </div>
       </Router>
     );
@@ -39,23 +26,3 @@ export default App;
 
 
 
-
-
-
-
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <p>
-      //       Edit <code>src/App.js</code> and save to reload.
-      //     </p>
-      //     <a
-      //       className="App-link"
-      //       href="https://reactjs.org"
-      //       target="_blank"
-      //       rel="noopener noreferrer"
-      //     >
-      //       Learn React
-      //     </a>
-      //   </header>
-      // </div>
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
+import App from "./App"
+import Person from './Person'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import contacts from './randomUsers'
+
+
 
 class Home extends Component {
 	render () {
 		return (
-			<h2>Home</h2>
+			<div id="list">
+	            <h3 id="h3">My Contacts</h3>
+	            <ul>
+	              {contacts.map(contact => (
+	                <li className="li" key={'contact' + contact.id}>
+	                  <Link to={"/Person/" + contact.id}><img id="thumbnail" src={contact.picture.thumbnail}/></Link>
+	                  <span className="idk"><Link to={"/Person/" + contact.id}>{contact.name.first + " " + contact.name.last}</Link></span>
+	                </li>
+	                ))}
+	            </ul>
+          	</div>
 		)
 	}
 }
